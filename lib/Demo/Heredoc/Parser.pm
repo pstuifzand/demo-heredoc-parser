2873025d51ab5cf2881a49643f6d7535a2ea995f
Peter Stuifzand <peter@stuifzand.eu>
2013-04-15 10:56:43 +0200

    Parse multiple heredocs on a line

--- lib/Demo/Heredoc/Parser.pm
+++ lib/Demo/Heredoc/Parser.pm
@@ -9,22 +9,28 @@ sub new {
         default_action => '::array',
 
         source => \<<'GRAMMAR',
+
 :start        ::= file
 
-file          ::= heredoc
+file          ::= expressions         (semi_colon newline)   action => ::first
 
-heredoc       ::= var ('=' marker name) literal (name)
+expressions   ::= expression+          separator => comma
 
-:lexeme         ~ marker pause => after
-:lexeme         ~ literal
+expression    ::= heredoc              action => ::first
+
+heredoc       ::= (marker) literal     action => ::first
+
+:lexeme         ~ marker     pause => before
+:lexeme         ~ newline    pause => before
 
 marker          ~ '<<'
-name            ~ [A-Z]+
-var             ~ [a-z]+
+semi_colon      ~ ';'
+newline         ~ [\n]
 literal         ~ [.]
+comma           ~ ','
 
-:discard   ~ ws
-ws         ~ [\s]+
+#:discard        ~ ws
+#ws              ~ [\s]+
 
 GRAMMAR
     });
@@ -39,22 +45,35 @@ sub parse {
     my ($self, $input) = @_;
     my %options = (
         #trace_values    => 1,
-        #trace_terminals => 1,
+        trace_terminals => 1,
     );
 
     my $re = Marpa::R2::Scanless::R->new({ %options, grammar => $self->{grammar} });
     my $pos = $re->read(\$input);
-    die if $pos < 0;
+    die "error" if $pos < 0;
 
-    if ($pos < length $input) {
+    my $last_heredoc_end;
+
+    while ($pos < length $input) {
         pos($input) = $pos;
-        my ($name) = $input =~ m/\G(\w+);\n/gc;
-        my $s = pos($input);
-        $re->lexeme_read('name', $pos, length($name), $name) // die;
-        my ($literal) = $input =~ m/\G(.+)^$name\n/gmsc;
-        $re->lexeme_read('literal', $s, length($literal), $literal) // die $re->show_progress;
-        $re->lexeme_read('name', pos($input)-length($name)-1, length($name), $name) // die $re->show_progress;
-        $re->resume(pos($input));
+
+        $last_heredoc_end //= index($input, "\n", $pos) + 1;
+
+        if (my ($name) = $input =~ m/\G<<(\w+)/msgc) {
+            my $s = pos($input);
+            pos($input) = $last_heredoc_end;
+            my ($literal) = $input =~ m/\G(.+)^$name\n/gmsc;
+            $re->lexeme_read('marker', $pos, 2, '<<') // die $re->show_progress;
+            $re->lexeme_read('literal', $last_heredoc_end, length($literal), $literal) // die $re->show_progress;
+            $last_heredoc_end = pos($input);
+            $pos = $re->resume($s);
+        }
+        elsif ($input =~ m/\G\n/gmsc) {
+            my $p = $last_heredoc_end;
+            $last_heredoc_end = undef;
+            $re->lexeme_read('newline', $pos, 1, "\n");
+            $pos = $re->resume($p);
+        }
     }
 
     my $v = $re->value;
--- t/parser.t
+++ t/parser.t
@@ -4,19 +4,24 @@ use Demo::Heredoc::Parser;
 my $p = Demo::Heredoc::Parser->new;
 
 my $v = $p->parse(<<"INPUT");
-x = <<END;
+<<ENDA,<<ENDB,<<ENDC;
 line 1
 line 2
 line 3
-END
+ENDA
+line 4
+line 5
+ENDB
+line 6
+line 7
+ENDC
 INPUT
 
 my $expected = [
-    [
-        'x', "line 1\nline 2\nline 3\n",
-    ],
+    "line 1\nline 2\nline 3\n",
+    "line 4\nline 5\n",
+    "line 6\nline 7\n",
 ];
-
 is_deeply($v, $expected);
 
 done_testing();
